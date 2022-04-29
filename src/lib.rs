acdfb4b281e398e6b9b206b3744a4537e72e34b7
redshiftzero <jen@penumbra.zone>
2022-04-29 16:49:47 -0400

    export `Bls12_377` parameter struct (implements `PairingEngine`) (#24)
    needed for https://github.com/penumbra-zone/penumbra/issues/711

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,7 @@ pub use encoding::Encoding;
 pub use error::EncodingError;
 pub use field_ext::FieldExt;
 
+pub use ark_bls12_377::Bls12_377;
 pub use ark_ed_on_bls12_377::{Fq, Fr};
 
 pub use invsqrt::SqrtRatioZeta;
