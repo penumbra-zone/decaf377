e17a3ada8674498a70c5df13fa5b7dd9fbd81080
redshiftzero <jen@penumbra.zone>
2022-10-28 10:40:23 -0400

    feat(groth16): more operations required for CurveVar trait

--- src/r1cs/ops.rs
+++ src/r1cs/ops.rs
@@ -12,8 +12,24 @@ impl Add for Decaf377ElementVar {
     }
 }
 
+impl<'a> Add<&'a Decaf377ElementVar> for Decaf377ElementVar {
+    type Output = Decaf377ElementVar;
+
+    fn add(self, other: Decaf377ElementVar) -> Self::Output {
+        Decaf377ElementVar {
+            inner: self.inner.add(other.inner),
+        }
+    }
+}
+
 impl AddAssign for Decaf377ElementVar {
     fn add_assign(&mut self, rhs: Decaf377ElementVar) {
+        self.inner.add_assign(rhs.inner)
+    }
+}
+
+impl<'a> AddAssign<&'a Decaf377ElementVar> for Decaf377ElementVar {
+    fn add_assign(&self, rhs: Decaf377ElementVar) {
         self.inner.add_assign(rhs.inner)
     }
 }
@@ -28,7 +44,23 @@ impl Sub for Decaf377ElementVar {
     }
 }
 
+impl<'a> Sub<&'a Decaf377ElementVar> for Decaf377ElementVar {
+    type Output = Decaf377ElementVar;
+
+    fn sub(self, other: Decaf377ElementVar) -> Self::Output {
+        Decaf377ElementVar {
+            inner: self.inner.sub(other.inner),
+        }
+    }
+}
+
 impl SubAssign for Decaf377ElementVar {
+    fn sub_assign(&mut self, rhs: Decaf377ElementVar) {
+        self.inner.sub_assign(rhs.inner)
+    }
+}
+
+impl<'a> SubAssign<&'a Decaf377ElementVar> for Decaf377ElementVar {
     fn sub_assign(&mut self, rhs: Decaf377ElementVar) {
         self.inner.sub_assign(rhs.inner)
     }
