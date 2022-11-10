52156c39e370ce19edeb66085e4cbe94a7a46ed2
redshiftzero <jen@penumbra.zone>
2022-11-10 14:19:52 -0500

    feat(groth16): remove `Decaf377...` prefix from R1CS element type

--- tests/groth16_gadgets.rs
+++ tests/groth16_gadgets.rs
@@ -7,7 +7,7 @@ use ark_r1cs_std::{
 };
 use ark_relations::r1cs::{ConstraintSynthesizer, ToConstraintField};
 use ark_snark::SNARK;
-use decaf377::{basepoint, r1cs::Decaf377ElementVar, Bls12_377, Element, Fq, Fr};
+use decaf377::{basepoint, r1cs::ElementVar, Bls12_377, Element, Fq, Fr};
 use rand_core::OsRng;
 
 struct DiscreteLogCircuit {
@@ -27,8 +27,8 @@ impl ConstraintSynthesizer<Fq> for DiscreteLogCircuit {
         let witness_vars = UInt8::new_witness_vec(cs.clone(), &self.scalar)?;
 
         // 2. Add public input variable
-        let public_var = Decaf377ElementVar::new_input(cs.clone(), || Ok(self.public))?;
-        let basepoint_var = Decaf377ElementVar::new_constant(cs, basepoint())?;
+        let public_var = ElementVar::new_input(cs.clone(), || Ok(self.public))?;
+        let basepoint_var = ElementVar::new_constant(cs, basepoint())?;
         // 3. Add constraint that scalar * Basepoint = public
         let test_public = basepoint_var.scalar_mul_le(witness_vars.to_bits_le()?.iter())?;
         public_var.enforce_equal(&test_public)?;
@@ -49,7 +49,7 @@ impl DiscreteLogCircuit {
 }
 
 #[test]
-fn proof_happy_path() {
+fn groth16_dl_proof_happy_path() {
     let (pk, vk) = DiscreteLogCircuit::generate_test_parameters();
     let mut rng = OsRng;
 
@@ -74,7 +74,7 @@ fn proof_happy_path() {
 }
 
 #[test]
-fn proof_unhappy_path() {
+fn groth16_dl_proof_unhappy_path() {
     let (pk, vk) = DiscreteLogCircuit::generate_test_parameters();
     let mut rng = OsRng;
 
