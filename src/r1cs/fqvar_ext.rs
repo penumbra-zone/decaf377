402ad00db92545c90cae6afc324b0a2e391e76f6
redshiftzero <jen@penumbra.zone>
2022-11-11 14:01:52 -0500

    fix(groth16): R1CS square root must handle all cases, flattened

--- src/r1cs/fqvar_ext.rs
+++ src/r1cs/fqvar_ext.rs
@@ -1,13 +1,13 @@
 use ark_ed_on_bls12_377::constraints::FqVar;
-use ark_ff::One;
-use ark_r1cs_std::prelude::{AllocVar, FieldVar};
+use ark_ff::{One, Zero};
+use ark_r1cs_std::prelude::{AllocVar, Boolean, FieldVar};
 use ark_r1cs_std::R1CSVar;
 use ark_r1cs_std::{eq::EqGadget, prelude::AllocationMode};
 use ark_relations::ns;
 use ark_relations::r1cs::SynthesisError;
 
 use crate::sign::Sign;
-use crate::{Fq, SqrtRatioZeta};
+use crate::{constants::ZETA, Fq, SqrtRatioZeta};
 
 pub trait FqVarExtension: Sized {
     fn isqrt(x: Fq, x_var: FqVar) -> Result<(bool, Fq), SynthesisError>;
@@ -21,22 +21,44 @@ pub trait FqVarExtension: Sized {
 }
 
 impl FqVarExtension for FqVar {
-    /// Square root in R1CS
+    /// Inverse square root in R1CS
     ///
-    /// Returns the result of the sqrt computation (out of circuit)
-    /// and a boolean constraint that `y = sqrt(x)` is known.
+    /// Cases:
+    /// - Case 1: `(true, sqrt(num/den))` if `num` and `den` are both nonzero and `num/den` is square;
+    /// - Case 2: `(true, 0)` if `num` is zero;
+    /// - Case 3: `(false, 0)` if `den` is zero;
+    /// - Case 4: `(false, sqrt(zeta*num/den))` if `num` and `den` are both nonzero and `num/den` is nonsquare;
     fn isqrt(x: Fq, x_var: FqVar) -> Result<(bool, Fq), SynthesisError> {
         // Out of circuit sqrt computation
-        let (was_square, y) = Fq::sqrt_ratio_zeta(&x, &Fq::one());
+        let (was_square, y) = Fq::sqrt_ratio_zeta(&Fq::one(), &x);
 
-        // Add constraints to certify we know the square root of x
-        let lhs = y * y;
-        let lhs_var = FqVar::new_variable(
-            ns!(x_var.cs(), "lhs_sqrt"),
-            || Ok(lhs),
-            AllocationMode::Witness,
-        )?;
-        lhs_var.enforce_equal(&x_var)?;
+        let cs = x_var.cs();
+        let was_square_var = Boolean::new_witness(cs.clone(), || Ok(was_square))?;
+        let y_squared_var = FqVar::new_witness(cs.clone(), || Ok(y * y))?;
+
+        // The below is a flattened version of the four cases above, excluding case 2 since `num` is hardcoded
+        // to be one.
+        //
+        // Case 3: `(false, 0)` if `den` is zero
+        let was_not_square_var = was_square_var.not();
+        let x_var_is_zero = x_var.is_eq(&FqVar::zero())?;
+        let in_case_3 = was_not_square_var.and(&x_var_is_zero)?;
+        // Certify the return value y is 0.
+        y_squared_var.conditional_enforce_equal(&FqVar::zero(), &in_case_3)?;
+
+        // Case 1: `(true, sqrt(num/den))` if `num` and `den` are both nonzero and `num/den` is square
+        let x_var_inv = x_var.inverse()?;
+        let in_case_1 = was_square_var;
+        // Certify the return value y is sqrt(1/x)
+        y_squared_var.conditional_enforce_equal(&x_var_inv, &in_case_1)?;
+
+        // Case 4: `(false, sqrt(zeta*num/den))` if `num` and `den` are both nonzero and `num/den` is nonsquare;
+        let zeta_var = FqVar::new_constant(cs, *ZETA)?;
+        let zeta_times_one_over_x_var = zeta_var * x_var_inv;
+        let in_case_4 = was_not_square_var.and(&x_var_is_zero.not())?;
+        // Certify the return value y is sqrt(zeta * 1/x)
+        y_squared_var.conditional_enforce_equal(&zeta_times_one_over_x_var, &in_case_4)?;
+
         Ok((was_square, y))
     }
 
