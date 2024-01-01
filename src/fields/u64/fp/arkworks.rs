ea2c6743c0c25476cee8255bc33ccec639c928d2
Tal Derei <talderei99@gmail.com>
2024-01-01 15:29:34 -0800

    porting trait impl and bounds from arkworks

--- src/fields/u64/fp/arkworks.rs
+++ src/fields/u64/fp/arkworks.rs
@@ -1,1 +1,664 @@
+use core::{ops::{Neg, Add, AddAssign, Sub, SubAssign, MulAssign, Mul, DivAssign, Div}, iter, fmt::{Formatter, Display}, cmp::Ordering};
 
+use ark_ff::{PrimeField, BigInt, Field, SqrtPrecomputation};
+use ark_serialize::{Flags, CanonicalDeserializeWithFlags, SerializationError, Compress, Validate, CanonicalDeserialize, Valid, CanonicalSerializeWithFlags, CanonicalSerialize};
+use ark_std::{str::FromStr, Zero, One, rand};
+use ark_ff::FftField;
+use core::hash::Hash;
+
+use super::wrapper::Fp;
+
+impl PrimeField for Fp {
+    type BigInt = BigInt<6>; // change from hardcoded
+    const MODULUS: Self::BigInt = ark_ff::BigInt([0; 6]); // use Fp::Modulus
+    const MODULUS_MINUS_ONE_DIV_TWO: Self::BigInt = unimplemented!(); // Fp::MODULUS.divide_by_2_round_down()
+    const MODULUS_BIT_SIZE: u32 = unimplemented!(); // Fp::MODULUS.const_num_bits()
+    const TRACE: Self::BigInt = unimplemented!(); // Fp::MODULUS.two_adic_coefficient()
+    const TRACE_MINUS_ONE_DIV_TWO: Self::BigInt = unimplemented!(); // Self::TRACE.divide_by_2_round_down()
+
+    fn from_bigint(repr: Self::BigInt) -> Option<Self> {
+        unimplemented!()
+    }
+
+    fn into_bigint(self) -> Self::BigInt {
+        unimplemented!()
+    }
+
+    fn from_be_bytes_mod_order(bytes: &[u8]) -> Self {
+       unimplemented!()
+    }
+
+    fn from_le_bytes_mod_order(bytes: &[u8]) -> Self {
+        unimplemented!()
+    }
+}
+
+impl Field for Fp {
+    type BasePrimeField = Self;
+    type BasePrimeFieldIter = iter::Once<Self::BasePrimeField>;
+
+    const SQRT_PRECOMP: Option<SqrtPrecomputation<Self>> = None; // use Fp::SQRT_PRECOMP
+    const ZERO: Self = unimplemented!();
+    const ONE: Self = unimplemented!();
+
+    fn extension_degree() -> u64 {
+        unimplemented!()
+    }
+
+    fn to_base_prime_field_elements(&self) -> Self::BasePrimeFieldIter {
+        unimplemented!()
+    }
+
+    fn from_base_prime_field_elems(elems: &[Self::BasePrimeField]) -> Option<Self> {
+        todo!()
+    }
+
+    fn from_base_prime_field(elem: Self::BasePrimeField) -> Self {
+        unimplemented!()
+    }
+
+    fn double(&self) -> Self {
+        unimplemented!()
+    }
+
+    fn double_in_place(&mut self) -> &mut Self {
+        unimplemented!()
+    }
+
+    // Should this be FpTrait invocation?
+    fn neg_in_place(&mut self) -> &mut Self {
+        unimplemented!()
+    }
+
+    fn from_random_bytes_with_flags<F: ark_serialize::Flags>(bytes: &[u8]) -> Option<(Self, F)> {
+        unimplemented!()
+    }
+
+    fn legendre(&self) -> ark_ff::LegendreSymbol {
+        unimplemented!()
+    }
+
+    fn square(&self) -> Self {
+        unimplemented!()
+    }
+
+    fn square_in_place(&mut self) -> &mut Self {
+        unimplemented!()
+    }
+
+    fn inverse(&self) -> Option<Self> {
+        unimplemented!()
+    }
+
+    fn inverse_in_place(&mut self) -> Option<&mut Self> {
+        unimplemented!()
+    }
+
+    fn frobenius_map_in_place(&mut self, power: usize) {
+        unimplemented!()
+    }
+
+    fn characteristic() -> &'static [u64] {
+        unimplemented!()
+    }
+
+    fn from_random_bytes(bytes: &[u8]) -> Option<Self> {
+        unimplemented!()
+    }
+
+    fn sqrt(&self) -> Option<Self> {
+        unimplemented!()
+    }
+
+    fn sqrt_in_place(&mut self) -> Option<&mut Self> {
+        unimplemented!()
+    }
+
+    fn sum_of_products<const T: usize>(a: &[Self; T], b: &[Self; T]) -> Self {
+        unimplemented!()
+    }
+
+    fn frobenius_map(&self, power: usize) -> Self {
+        unimplemented!()
+    }
+
+    fn pow<S: AsRef<[u64]>>(&self, exp: S) -> Self {
+        unimplemented!()
+    }
+
+    fn pow_with_table<S: AsRef<[u64]>>(powers_of_2: &[Self], exp: S) -> Option<Self> {
+        unimplemented!()
+    }
+}
+
+impl From<u128> for Fp {
+    fn from(value: u128) -> Self {
+        unimplemented!()
+    }
+}
+
+impl From<u64> for Fp {
+    fn from(value: u64) -> Self {
+        unimplemented!()
+    }
+}
+
+impl From<u32> for Fp {
+    fn from(value: u32) -> Self {
+        unimplemented!()
+    }
+}
+
+impl From<u16> for Fp {
+    fn from(value: u16) -> Self {
+        unimplemented!()
+    }
+}
+
+impl From<u8> for Fp {
+    fn from(value: u8) -> Self {
+        unimplemented!()
+    }
+}
+
+impl From<bool> for Fp {
+    fn from(value: bool) -> Self {
+        unimplemented!()
+    }
+}
+
+impl Neg for Fp {
+    type Output = Self;
+
+    #[inline]
+    #[must_use]
+    fn neg(mut self) -> Self {
+        Fp::neg_in_place(&mut self);
+        self
+    }
+}
+
+//////////////////////
+
+impl<'a> AddAssign<&'a Self> for Fp {
+    #[inline]
+    fn add_assign(&mut self, other: &Self) {
+        Fp::add_assign(self, other)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl core::ops::AddAssign<Self> for Fp {
+    #[inline(always)]
+    fn add_assign(&mut self, other: Self) {
+        self.add_assign(&other)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a> core::ops::AddAssign<&'a mut Self> for Fp {
+    #[inline(always)]
+    fn add_assign(&mut self, other: &'a mut Self) {
+        self.add_assign(&*other)
+    }
+}
+
+//////////////////////
+
+#[allow(unused_qualifications)]
+impl core::ops::Add<Self> for Fp {
+    type Output = Self;
+
+    #[inline]
+    fn add(mut self, other: Self) -> Self {
+        self.add_assign(&other);
+        self
+    }
+}
+
+impl<'a> Add<&'a Fp> for Fp {
+    type Output = Self;
+
+    #[inline]
+    fn add(mut self, other: &Self) -> Self {
+        self.add_assign(other);
+        self
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a> core::ops::Add<&'a mut Self> for Fp {
+    type Output = Self;
+
+    #[inline]
+    fn add(mut self, other: &'a mut Self) -> Self {
+        self.add_assign(&*other);
+        self
+    }
+}
+
+//////////////////////
+
+impl<'a> SubAssign<&'a Self> for Fp {
+    #[inline]
+    fn sub_assign(&mut self, other: &Self) {
+        Fp::sub_assign(self, other);
+    }
+}
+
+#[allow(unused_qualifications)]
+impl core::ops::SubAssign<Self> for Fp {
+    #[inline(always)]
+    fn sub_assign(&mut self, other: Self) {
+        self.sub_assign(&other)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a> core::ops::SubAssign<&'a mut Self> for Fp {
+    #[inline(always)]
+    fn sub_assign(&mut self, other: &'a mut Self) {
+        self.sub_assign(&*other)
+    }
+}
+
+//////////////////////
+
+#[allow(unused_qualifications)]
+impl core::ops::Sub<Self> for Fp {
+    type Output = Self;
+
+    #[inline]
+    fn sub(mut self, other: Self) -> Self {
+        self.sub_assign(&other);
+        self
+    }
+}
+
+impl<'a> Sub<&'a Fp> for Fp {
+    type Output = Self;
+
+    #[inline]
+    fn sub(mut self, other: &Self) -> Self {
+        self.sub_assign(other);
+        self
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a> core::ops::Sub<&'a mut Self> for Fp {
+    type Output = Self;
+
+    #[inline]
+    fn sub(mut self, other: &'a mut Self) -> Self {
+        self.sub_assign(&*other);
+        self
+    }
+}
+
+//////////////////////
+
+impl<'a> MulAssign<&'a Self> for Fp {
+    fn mul_assign(&mut self, other: &Self) {
+        Fp::mul_assign(self, other)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl core::ops::MulAssign<Self> for Fp {
+    #[inline(always)]
+    fn mul_assign(&mut self, other: Self) {
+        self.mul_assign(&other)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a> core::ops::MulAssign<&'a mut Self> for Fp {
+    #[inline(always)]
+    fn mul_assign(&mut self, other: &'a mut Self) {
+        self.mul_assign(&*other)
+    }
+}
+
+//////////////////////
+
+#[allow(unused_qualifications)]
+impl core::ops::Mul<Self> for Fp {
+    type Output = Self;
+
+    #[inline(always)]
+    fn mul(mut self, other: Self) -> Self {
+        self.mul_assign(&other);
+        self
+    }
+}
+
+impl<'a> Mul<&'a Fp> for Fp {
+    type Output = Self;
+
+    #[inline]
+    fn mul(mut self, other: &Self) -> Self {
+        self.mul_assign(other);
+        self
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a> core::ops::Mul<&'a mut Self> for Fp {
+    type Output = Self;
+
+    #[inline(always)]
+    fn mul(mut self, other: &'a mut Self) -> Self {
+        self.mul_assign(&*other);
+        self
+    }
+}
+
+//////////////////////
+
+/// Computes `self *= other.inverse()` if `other.inverse()` is `Some`, and
+/// panics otherwise.
+impl<'a> DivAssign<&'a Self> for Fp {
+    #[inline(always)]
+    fn div_assign(&mut self, other: &Self) {
+        self.mul_assign(&other.inverse().unwrap());
+    }
+}
+
+#[allow(unused_qualifications)]
+impl core::ops::DivAssign<Self> for Fp {
+    #[inline(always)]
+    fn div_assign(&mut self, other: Self) {
+        self.div_assign(&other)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a> core::ops::DivAssign<&'a mut Self> for Fp {
+    #[inline(always)]
+    fn div_assign(&mut self, other: &'a mut Self) {
+        self.div_assign(&*other)
+    }
+}
+
+//////////////////////
+
+#[allow(unused_qualifications)]
+impl core::ops::Div<Self> for Fp {
+    type Output = Self;
+
+    #[inline(always)]
+    fn div(mut self, other: Self) -> Self {
+        self.div_assign(&other);
+        self
+    }
+}
+
+impl<'a> Div<&'a Fp> for Fp {
+    type Output = Self;
+
+    /// Returns `self * other.inverse()` if `other.inverse()` is `Some`, and
+    /// panics otherwise.
+    #[inline]
+    fn div(mut self, other: &Self) -> Self {
+        self.mul_assign(&other.inverse().unwrap());
+        self
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a> core::ops::Div<&'a mut Self> for Fp {
+    type Output = Self;
+
+    #[inline(always)]
+    fn div(mut self, other: &'a mut Self) -> Self {
+        self.div_assign(&*other);
+        self
+    }
+}
+
+//////////////////////
+
+impl Zero for Fp {
+    #[inline]
+    fn zero() -> Self {
+        Fp::ZERO
+    }
+
+    #[inline]
+    fn is_zero(&self) -> bool {
+        *self == Fp::ZERO
+    }
+}
+
+#[allow(unused_qualifications)]
+impl core::iter::Sum<Self> for Fp {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Self::zero(), core::ops::Add::add)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a> core::iter::Sum<&'a Self> for Fp {
+    fn sum<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
+        iter.fold(Self::zero(), core::ops::Add::add)
+    }
+}
+
+impl One for Fp {
+    #[inline]
+    fn one() -> Self {
+        Fp::ONE
+    }
+
+    #[inline]
+    fn is_one(&self) -> bool {
+        *self == Fp::ONE
+    }
+}
+
+#[allow(unused_qualifications)]
+impl core::iter::Product<Self> for Fp {
+    fn product<I: Iterator<Item = Self>>(iter: I) -> Self {
+        iter.fold(Self::one(), core::ops::Mul::mul)
+    }
+}
+
+#[allow(unused_qualifications)]
+impl<'a> core::iter::Product<&'a Self> for Fp {
+    fn product<I: Iterator<Item = &'a Self>>(iter: I) -> Self {
+        iter.fold(Self::one(), Mul::mul)
+    }
+}
+
+//////////////////////
+
+impl CanonicalDeserializeWithFlags for Fp {
+    fn deserialize_with_flags<R: ark_std::io::Read, F: Flags>(
+        reader: R,
+    ) -> Result<(Self, F), SerializationError> {
+        unimplemented!()
+    }
+}
+
+impl Valid for Fp {
+    fn check(&self) -> Result<(), SerializationError> {
+        unimplemented!()
+    }
+}
+
+impl CanonicalDeserialize for Fp {
+    fn deserialize_with_mode<R: ark_std::io::Read>(
+        reader: R,
+        _compress: Compress,
+        _validate: Validate,
+    ) -> Result<Self, SerializationError> {
+        unimplemented!()
+    }
+}
+
+impl CanonicalSerialize for Fp {
+    #[inline]
+    fn serialize_with_mode<W: ark_std::io::Write>(
+        &self,
+        writer: W,
+        _compress: Compress,
+    ) -> Result<(), SerializationError> {
+        unimplemented!()
+    }
+
+    #[inline]
+    fn serialized_size(&self, _compress: Compress) -> usize {
+        unimplemented!()
+    }
+}
+
+impl CanonicalSerializeWithFlags for Fp {
+    fn serialize_with_flags<W: ark_std::io::Write, F: Flags>(
+        &self,
+        writer: W,
+        flags: F,
+    ) -> Result<(), SerializationError> {
+        unimplemented!()
+    }
+
+    // Let `m = 8 * n` for some `n` be the smallest multiple of 8 greater
+    // than `Fp::MODULUS_BIT_SIZE`.
+    // If `(m - Fp::MODULUS_BIT_SIZE) >= F::BIT_SIZE` , then this method returns `n`;
+    // otherwise, it returns `n + 1`.
+    fn serialized_size_with_flags<F: Flags>(&self) -> usize {
+        unimplemented!()
+    }
+}
+
+impl ark_std::rand::distributions::Distribution<Fp>
+    for ark_std::rand::distributions::Standard
+{
+    fn sample<R: rand::prelude::Rng + ?Sized>(&self, rng: &mut R) -> Fp {
+        todo!()
+    }
+}
+
+//////////////////////
+
+impl ark_std::fmt::Debug for Fp {
+    fn fmt(&self, f: &mut Formatter<'_>) -> ark_std::fmt::Result {
+        unimplemented!()
+    }
+}
+
+/// Outputs a string containing the value of `self`,
+/// represented as a decimal without leading zeroes.
+impl Display for Fp {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        unimplemented!()
+    }
+}
+
+impl zeroize::Zeroize for Fp {
+    // The phantom data does not contain element-specific data
+    // and thus does not need to be zeroized.
+    fn zeroize(&mut self) {
+        unimplemented!()
+    }
+}
+
+/// Note that this implementation of `Ord` compares field elements viewing
+/// them as integers in the range 0, 1, ..., Fp::MODULUS - 1. However, other
+/// implementations of `PrimeField` might choose a different ordering, and
+/// as such, users should use this `Ord` for applications where
+/// any ordering suffices (like in a BTreeMap), and not in applications
+/// where a particular ordering is required.
+impl Ord for Fp {
+    #[inline(always)]
+    fn cmp(&self, other: &Self) -> Ordering {
+        unimplemented!()
+    }
+}
+
+/// Note that this implementation of `PartialOrd` compares field elements
+/// viewing them as integers in the range 0, 1, ..., `Fp::MODULUS` - 1. However,
+/// other implementations of `PrimeField` might choose a different ordering, and
+/// as such, users should use this `PartialOrd` for applications where
+/// any ordering suffices (like in a BTreeMap), and not in applications
+/// where a particular ordering is required.
+impl PartialOrd for Fp {
+    #[inline(always)]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        unimplemented!()
+    }
+}
+
+//////////////////////
+
+impl FromStr for Fp {
+    type Err = ();
+
+    /// Interpret a string of numbers as a (congruent) prime field element.
+    /// Does not accept unnecessary leading zeroes or a blank string.
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        unimplemented!()
+    }
+}
+
+impl From<num_bigint::BigUint> for Fp {
+    #[inline]
+    fn from(val: num_bigint::BigUint) -> Fp {
+        unimplemented!()
+    }
+}
+
+impl From<Fp> for num_bigint::BigUint {
+    #[inline(always)]
+    fn from(other: Fp) -> Self {
+        unimplemented!()
+    }
+}
+
+impl From<Fp> for BigInt<6> {
+    #[inline(always)]
+    fn from(fp: Fp) -> Self {
+        unimplemented!()
+    }
+}
+
+impl From<BigInt<6>> for Fp {
+    /// Converts `Self::BigInteger` into `Self`
+    #[inline(always)]
+    fn from(int: BigInt<6>) -> Self {
+        unimplemented!()
+    }
+}
+
+impl FftField for Fp {
+    const GENERATOR: Self = unimplemented!(); // use Fp::GENERATOR
+    const TWO_ADICITY: u32 = unimplemented!(); // use Fp::TWO_ADICITY
+    const TWO_ADIC_ROOT_OF_UNITY: Self = unimplemented!(); // Fp::TWO_ADIC_ROOT_OF_UNITY
+    const SMALL_SUBGROUP_BASE: Option<u32> = unimplemented!(); // Fp::SMALL_SUBGROUP_BASE
+    const SMALL_SUBGROUP_BASE_ADICITY: Option<u32> = unimplemented!(); // Fp::SMALL_SUBGROUP_BASE_ADICITY
+    const LARGE_SUBGROUP_ROOT_OF_UNITY: Option<Self> = unimplemented!(); // Fp::LARGE_SUBGROUP_ROOT_OF_UNITY
+
+    fn get_root_of_unity(n: u64) -> Option<Self> {
+        unimplemented!()
+    }
+}
+
+impl Hash for Fp {
+    fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
+        unimplemented!()
+    }
+}
+
+impl PartialEq for Fp {
+    fn eq(&self, other: &Self) -> bool {
+        unimplemented!()
+    }
+}
+
+impl Eq for Fp {}
+
+impl Default for Fp {
+    fn default() -> Self {
+        unimplemented!()
+    }
+}
--- src/fields/u64/fp/wrapper.rs
+++ src/fields/u64/fp/wrapper.rs
@@ -1,29 +1,169 @@
-use core::ops::{Add, Mul, Neg, Sub};
+use core::{ops::{Add, Mul, Neg, Sub}, marker::PhantomData};
 
-use super::fiat;
+use super::fiat::{self, FpMontgomeryDomainFieldElement};
+use ark_ff::{BigInt, SqrtPrecomputation};
+use ark_std::{
+    hash::Hash,
+};
+use derivative::Derivative;
 
 #[derive(Copy, Clone)]
-pub struct Fp(fiat::FpMontgomeryDomainFieldElement);
+pub struct Fp(FpMontgomeryDomainFieldElement);
+    
+pub trait FpTrait: Send + Sync + 'static + Sized {
+    // fn from_bytes(bytes: &[u8; 48]) -> Self;
+    // fn to_bytes(&self) -> [u8; 48];
+    // fn zero() -> Self;
+    // fn one() -> Self;
+    // fn square(&self) -> Self;
 
-impl core::fmt::Debug for Fp {
-    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-        let bytes = self.to_bytes();
-        write!(f, "Fp(0x{})", hex::encode(bytes))
-    }
+    /// The modulus of the field.
+    const MODULUS: BigInt<6>;
+
+    /// A multiplicative generator of the field.
+    /// `Self::GENERATOR` is an element having multiplicative order
+    /// `Self::MODULUS - 1`.
+    const GENERATOR: Fp;
+
+    /// Additive identity of the field, i.e. the element `e`
+    /// such that, for all elements `f` of the field, `e + f = f`.
+    const ZERO: Fp;
+
+    /// Multiplicative identity of the field, i.e. the element `e`
+    /// such that, for all elements `f` of the field, `e * f = f`.
+    const ONE: Fp;
+
+    /// Let `N` be the size of the multiplicative group defined by the field.
+    /// Then `TWO_ADICITY` is the two-adicity of `N`, i.e. the integer `s`
+    /// such that `N = 2^s * t` for some odd integer `t`.
+    const TWO_ADICITY: u32;
+
+    /// 2^s root of unity computed by GENERATOR^t
+    const TWO_ADIC_ROOT_OF_UNITY: Fp;
+
+    /// An integer `b` such that there exists a multiplicative subgroup
+    /// of size `b^k` for some integer `k`.
+    const SMALL_SUBGROUP_BASE: Option<u32> = None;
+
+    /// The integer `k` such that there exists a multiplicative subgroup
+    /// of size `Self::SMALL_SUBGROUP_BASE^k`.
+    const SMALL_SUBGROUP_BASE_ADICITY: Option<u32> = None;
+
+    /// GENERATOR^((MODULUS-1) / (2^s *
+    /// SMALL_SUBGROUP_BASE^SMALL_SUBGROUP_BASE_ADICITY)) Used for mixed-radix
+    /// FFT.
+    const LARGE_SUBGROUP_ROOT_OF_UNITY: Option<Fp> = None;
+
+    // Precomputed material for use when computing square roots.
+    // Currently uses the generic Tonelli-Shanks,
+    // which works for every modulus.
+    const SQRT_PRECOMP: Option<SqrtPrecomputation<Fp>>;
+
+    /// Set a += b.
+    fn add_assign(self, other: Fp) -> Fp;
+
+    /// Set a -= b.
+    fn sub_assign(self, other: Fp) -> Fp;
+
+    /// Set a = a + a.
+    fn double_in_place(a: &mut Fp);
+
+    /// Set a = -a;
+    fn neg_in_place(self) -> Fp;
+
+    /// Set a *= b.
+    fn mul_assign(self, other: Fp) -> Fp;
+
+    /// Compute the inner product `<a, b>`.
+    fn sum_of_products<const T: usize>(a: &[Fp; T], b: &[Fp; T]) -> Fp;
+
+    /// Set a *= b.
+    fn square_in_place(&self) -> Self;
+
+    /// Compute a^{-1} if `a` is not zero.
+    fn inverse(a: &Fp) -> Option<Fp>;
+
+    /// Construct a field element from an integer in the range
+    /// `0..(Self::MODULUS - 1)`. Returns `None` if the integer is outside
+    /// this range.
+    fn from_bigint(other: BigInt<6>) -> Option<Fp>;
+
+    /// Convert a field element to an integer in the range `0..(Self::MODULUS -
+    /// 1)`.
+    fn into_bigint(other: Fp) -> BigInt<6>;
+
+    fn from_bytes(bytes: &[u8; 48]) -> Self;
+
+    fn to_bytes(&self) -> [u8; 48];
+
+    fn zero() -> Self;
+
+    fn one() -> Self;
 }
 
-impl PartialEq for Fp {
-    fn eq(&self, other: &Fp) -> bool {
-        let self_bytes = self.to_bytes();
-        let other_bytes = other.to_bytes();
-        self_bytes[..] == other_bytes[..]
+impl FpTrait for Fp {
+    const MODULUS: BigInt<6> = ark_ff::BigInt([0; 6]);
+    const GENERATOR: Fp = Fp(FpMontgomeryDomainFieldElement([0; 6]));
+    const ZERO: Fp = Fp(FpMontgomeryDomainFieldElement([0; 6]));
+    const ONE: Fp = Fp(FpMontgomeryDomainFieldElement([0; 6]));
+    const TWO_ADICITY: u32 = 0;
+    const TWO_ADIC_ROOT_OF_UNITY: Fp = Fp(FpMontgomeryDomainFieldElement([0; 6]));
+    const SMALL_SUBGROUP_BASE: Option<u32> = None;
+    const SMALL_SUBGROUP_BASE_ADICITY: Option<u32> = None;
+    const LARGE_SUBGROUP_ROOT_OF_UNITY: Option<Fp> = None;
+    const SQRT_PRECOMP: Option<SqrtPrecomputation<Fp>> = None;
+
+    fn add_assign(self, other: Fp) -> Fp {
+        let mut result = fiat::FpMontgomeryDomainFieldElement([0; 6]);
+        fiat::fp_add(&mut result, &self.0, &other.0);
+        Fp(result)
     }
-}
 
-impl Eq for Fp {}
+    fn sub_assign(self, other: Fp) -> Fp {
+        let mut result = fiat::FpMontgomeryDomainFieldElement([0; 6]);
+        fiat::fp_sub(&mut result, &self.0, &other.0);
+        Fp(result)
+    }
 
-impl Fp {
-    pub fn from_bytes(bytes: &[u8; 48]) -> Self {
+    fn double_in_place(a: &mut Fp) {
+        unimplemented!()
+    }
+
+    fn neg_in_place(self) -> Fp {
+        let mut result = fiat::FpMontgomeryDomainFieldElement([0; 6]);
+        fiat::fp_opp(&mut result, &self.0);
+        Fp(result)
+    }
+
+    fn mul_assign(self, other: Fp) -> Fp {
+        let mut result = fiat::FpMontgomeryDomainFieldElement([0; 6]);
+        fiat::fp_mul(&mut result, &self.0, &other.0);
+        Fp(result)
+    }
+
+    fn sum_of_products<const T: usize>(a: &[Fp; T], b: &[Fp; T]) -> Fp {
+        unimplemented!()
+    }
+
+    fn square_in_place(&self) -> Self {
+        let mut result = fiat::FpMontgomeryDomainFieldElement([0; 6]);
+        fiat::fp_square(&mut result, &self.0);
+        Self(result)
+    }
+
+    fn inverse(a: &Fp) -> Option<Fp> {
+        unimplemented!()
+    }
+
+    fn from_bigint(other: BigInt<6>) -> Option<Fp> {
+        todo!()
+    }
+
+    fn into_bigint(other: Fp) -> BigInt<6> {
+        unimplemented!()
+    }
+
+    fn from_bytes(bytes: &[u8; 48]) -> Self {
         let mut x_non_montgomery = fiat::FpNonMontgomeryDomainFieldElement([0; 6]);
         let mut x = fiat::FpMontgomeryDomainFieldElement([0; 6]);
 
@@ -33,7 +173,7 @@ impl Fp {
         Self(x)
     }
 
-    pub fn to_bytes(&self) -> [u8; 48] {
+    fn to_bytes(&self) -> [u8; 48] {
         let mut x_non_montgomery = fiat::FpNonMontgomeryDomainFieldElement([0; 6]);
         let mut bytes = [0u8; 48];
 
@@ -43,59 +183,13 @@ impl Fp {
         bytes
     }
 
-    pub fn zero() -> Self {
+    fn zero() -> Self {
         Self(fiat::FpMontgomeryDomainFieldElement([0; 6]))
     }
 
-    pub fn one() -> Self {
+    fn one() -> Self {
         let mut one = Self::zero();
         fiat::fp_set_one(&mut one.0);
         one
-    }
-
-    pub fn square(&self) -> Self {
-        let mut result = fiat::FpMontgomeryDomainFieldElement([0; 6]);
-        fiat::fp_square(&mut result, &self.0);
-        Self(result)
-    }
-}
-
-impl Add<Fp> for Fp {
-    type Output = Fp;
-
-    fn add(self, other: Fp) -> Fp {
-        let mut result = fiat::FpMontgomeryDomainFieldElement([0; 6]);
-        fiat::fp_add(&mut result, &self.0, &other.0);
-        Fp(result)
-    }
-}
-
-impl Sub<Fp> for Fp {
-    type Output = Fp;
-
-    fn sub(self, other: Fp) -> Fp {
-        let mut result = fiat::FpMontgomeryDomainFieldElement([0; 6]);
-        fiat::fp_sub(&mut result, &self.0, &other.0);
-        Fp(result)
-    }
-}
-
-impl Mul<Fp> for Fp {
-    type Output = Fp;
-
-    fn mul(self, other: Fp) -> Fp {
-        let mut result = fiat::FpMontgomeryDomainFieldElement([0; 6]);
-        fiat::fp_mul(&mut result, &self.0, &other.0);
-        Fp(result)
-    }
-}
-
-impl Neg for Fp {
-    type Output = Fp;
-
-    fn neg(self) -> Fp {
-        let mut result = fiat::FpMontgomeryDomainFieldElement([0; 6]);
-        fiat::fp_opp(&mut result, &self.0);
-        Fp(result)
     }
 }
