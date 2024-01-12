5ef357fd03e5328336f55a616d7fbf404ed96cfe
Lucas Meier <lucas@cronokirby.com>
2024-01-12 10:17:36 -0800

    Make u64 a default feature

--- src/fields/fp.rs
+++ src/fields/fp.rs
@@ -8,7 +8,7 @@ pub mod u32;
 pub mod u64;
 
 #[cfg(all(not(feature = "u32"), not(feature = "u64")))]
-// compile_error!("No backend selected. Please select either: 'u32_backend' or 'u64_backend'.");
+compile_error!("No backend selected. Please select either: 'u32_backend' or 'u64_backend'.");
 #[cfg(all(feature = "u32", feature = "u64"))]
 compile_error!(
     "Multiple backends selected. Please select only one: 'u32_backend' or 'u64_backend'."
