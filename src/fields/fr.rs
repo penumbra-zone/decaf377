5fe41874858b20b23430bbf3278965e7cbeb5665
Lucas Meier <lucas@cronokirby.com>
2024-01-12 12:26:58 -0800

    Simplify backend feature selection to be about toggling u32

--- src/fields/fr.rs
+++ src/fields/fr.rs
@@ -7,13 +7,6 @@ pub mod arkworks;
 pub mod u32;
 pub mod u64;
 
-#[cfg(all(not(feature = "u32_backend"), not(feature = "u64_backend")))]
-// compile_error!("No backend selected. Please select either: 'u32_backend' or 'u64_backend'.");
-#[cfg(all(feature = "u32", feature = "u64"))]
-compile_error!(
-    "Multiple backends selected. Please select only one: 'u32_backend' or 'u64_backend'."
-);
-
 cfg_if! {
     if #[cfg(feature = "u32_backend")] {
         pub type Fr = u32::Fr;
