3440903f41341e9aa8506415d1a4f1f6200773f7
Lucas Meier <lucas@cronokirby.com>
2024-01-24 12:58:58 -0800

    Add feature gating for arkworks
    This also gates most of the crate, so that we have no-std compatability

--- src/lib.rs
+++ src/lib.rs
@@ -2,48 +2,54 @@
 //! `decaf377` [instantiates Decaf over the BLS12-377 scalar
 //! field](https://penumbra.zone/crypto/primitives/decaf377.html).
 //!
+use cfg_if::cfg_if;
 
 pub mod fields;
 
-pub mod bls12_377;
-mod constants;
-mod element;
-mod elligator;
-mod encoding;
-mod error;
-mod field_ext;
-mod invsqrt;
-mod on_curve;
-mod ops;
-pub mod rand;
-pub mod serialize;
-mod sign;
+cfg_if! {
+    if #[cfg(feature = "arkworks")] {
+        pub mod bls12_377;
+        mod constants;
+        mod element;
+        mod elligator;
+        mod encoding;
+        mod error;
+        mod field_ext;
+        mod invsqrt;
+        mod on_curve;
+        mod ops;
+        pub mod rand;
+        pub mod serialize;
+        mod sign;
 
-pub use constants::ZETA;
-pub use element::{AffineElement, Element};
-pub(crate) use element::{Decaf377EdwardsConfig, EdwardsProjective};
-pub use encoding::Encoding;
-pub use error::EncodingError;
-pub use field_ext::FieldExt;
+        pub use constants::ZETA;
+        pub use element::{AffineElement, Element};
+        pub(crate) use element::{Decaf377EdwardsConfig, EdwardsProjective};
+        pub use encoding::Encoding;
+        pub use error::EncodingError;
+        pub use field_ext::FieldExt;
 
-#[cfg(feature = "r1cs")]
-pub mod r1cs;
+        #[cfg(feature = "r1cs")]
+        pub mod r1cs;
 
-pub use bls12_377::Bls12_377;
-pub use fields::{fp::Fp, fq::Fq, fr::Fr};
+        pub use bls12_377::Bls12_377;
+        pub use fields::{fp::Fp, fq::Fq, fr::Fr};
 
-pub use invsqrt::SqrtRatioZeta;
-use on_curve::OnCurve;
-use sign::Sign;
+        pub use invsqrt::SqrtRatioZeta;
+        use on_curve::OnCurve;
+        use sign::Sign;
 
-/// Return the conventional generator for `decaf377`.
-pub fn basepoint() -> Element {
-    Element {
-        inner: EdwardsProjective::new(
-            *constants::B_X,
-            *constants::B_Y,
-            *constants::B_T,
-            *constants::B_Z,
-        ),
+        /// Return the conventional generator for `decaf377`.
+        pub fn basepoint() -> Element {
+            Element {
+                inner: EdwardsProjective::new(
+                    *constants::B_X,
+                    *constants::B_Y,
+                    *constants::B_T,
+                    *constants::B_Z,
+                ),
+            }
+        }
+    } else {
     }
 }
