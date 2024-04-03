396d40229035dae34c2acfec07db8c3a84abc26f
redshiftzero <jen@penumbralabs.xyz>
2024-04-03 10:31:56 -0400

    remove conflicting trait impl for Encoding

--- src/min_curve/encoding.rs
+++ src/min_curve/encoding.rs
@@ -1,31 +1,4 @@
 use crate::EncodingError;
-use core::convert::TryFrom;
 
 #[derive(Copy, Clone, Default, Eq, Ord, PartialOrd, PartialEq, Debug)]
 pub struct Encoding(pub [u8; 32]);
-
-impl TryFrom<&[u8]> for Encoding {
-    type Error = EncodingError;
-
-    fn try_from(bytes: &[u8]) -> Result<Self, Self::Error> {
-        if bytes.len() == 32 {
-            let mut arr = [0u8; 32];
-            arr.copy_from_slice(&bytes[0..32]);
-            Ok(Encoding(arr))
-        } else {
-            Err(EncodingError::InvalidSliceLength)
-        }
-    }
-}
-
-impl From<[u8; 32]> for Encoding {
-    fn from(bytes: [u8; 32]) -> Encoding {
-        Encoding(bytes)
-    }
-}
-
-impl From<Encoding> for [u8; 32] {
-    fn from(enc: Encoding) -> [u8; 32] {
-        enc.0
-    }
-}
